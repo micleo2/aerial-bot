d800847ecc3a79c86b2ce68d7831dc5d2209a1d9
Michael Leon <fbmal7@meta.com>
2023-12-03 20:42:21 -0800

    working prototype of nlp rocket 1 waypoint no gravity

--- py/nlp_rocket.py
+++ py/nlp_rocket.py
@@ -1,6 +1,6 @@
 # from casadi import MX, SX, vertcat, mtimes, Function, sqrt, norm_2
-from casadi import *
 from p5 import *
+import casadi as ca
 
 rocket = None
 path = None
@@ -10,8 +10,10 @@ U_sol = None
 u_idx = None
 wp_thresh = 50
 
-GRAVITY = 0.175
-BOOST_ACCEL = 0.4
+# GRAVITY = 0.175
+GRAVITY = 0
+# BOOST_ACCEL = 0.4
+BOOST_ACCEL = 0.1
 YAW_ACCEL = 0.008
 ANG_VEL_DAMPING = 0.90
 MAX_ANG_VEL = 0.15
@@ -31,33 +33,35 @@ def key_pressed():
 def key_released():
     global keys
     keys[key.name.upper()] = False
-    # if key.name.upper() == "S":
-    #     simple_input(path.get_next_n(2))
+    if key.name.upper() == "S":
+        solver_input(path.get_next_n(2))
+    if key.name.upper() == "R":
+        rocket.pos.x = width / 2
+        rocket.pos.y = height / 2
 def key_is_down(k):
     global keys
     return keys.get(str(k).upper(), False)
 
-def simple_input(waypnts):
-    global pos, vel, accel
-    global vel_limit, accel_thrust, vel_dampening
+def solver_input(waypnts):
+    global rocket
     # Number of frames to compute ahead of time, the horizon width.
-    N = 250 
-    opti = Opti()
+    N = 200
+    opti = ca.Opti()
 
     # ---- decision variables ---------
     # state trajectory
-    X       = opti.variable(4,N+1)
-    posx    = X[0,:]
-    posy    = X[1,:]
-    velx    = X[2,:]
-    vely    = X[3,:]
-    progress = opti.variable(N)
+    X       = opti.variable(6,N+1)
+    posx      = X[0,:]
+    posy      = X[1,:]
+    velx      = X[2,:]
+    vely      = X[3,:]
+    theta     = X[4,:]
+    theta_dot = X[5,:]
     # ---- control variables ---------
-    # acceleration x,y
+    # yaw, hold_boost
     U = opti.variable(2,N)
-    Ux = U[0,:]
-    Uy = U[1,:]
-
+    Uyaw = U[0,:]
+    Uboost = U[1,:]
 
     # ---- objective          ---------
     opti.minimize((posx[-1] - waypnts[0].x) ** 2 + (posy[-1] - waypnts[0].y) ** 2)
@@ -69,161 +73,75 @@ def simple_input(waypnts):
        posy_k = x_k[1]
        velx_k = x_k[2]
        vely_k = x_k[3]
+       theta_k = x_k[4]
+       theta_dot_k = x_k[5]
        U_k = U[:,k]
-       Ux_k = U_k[0]
-       Uy_k = U_k[1]
-       x_next = vertcat(
+       Uyaw_k = U_k[0]
+       Uboost_k = U_k[1]
+       # step_boost = 1 / (1 + ca.exp(-Uboost_k))
+       step_boost = 0.5 + 0.5 * (ca.tanh(500 * Uboost_k - 3))
+       velx_next = velx_k + step_boost * ca.cos(theta_k) * BOOST_ACCEL
+       vely_next = vely_k + step_boost * -ca.sin(theta_k) * BOOST_ACCEL
+       theta_dot_next = (theta_dot_k + Uyaw_k * YAW_ACCEL) * ANG_VEL_DAMPING
+       x_next = ca.vertcat(
                posx_k + velx_k,
                posy_k + vely_k,
-               (velx_k + Ux_k) * vel_dampening,
-               (vely_k + Uy_k) * vel_dampening)
+               velx_next,
+               vely_next,
+               theta_k + theta_dot_k,
+               theta_dot_next)
        # Apply dynamics as a constraint
        opti.subject_to(X[:,k+1] == x_next)
-       # Enforce physics limits
-       opti.subject_to((velx_k ** 2 + vely_k ** 2) <= vel_limit ** 2)
-       # Enforce actuator limits
-       opti.subject_to((Ux_k ** 2 + Uy_k ** 2) <= accel_thrust ** 2)
-       #progress
-       # cur_dist = (posx_k - waypnts[0].x) ** 2 + (posy_k - waypnts[0].y) ** 2
-       # opti.subject_to()
+       # Enforce limits
+       opti.subject_to((velx_k ** 2 + vely_k ** 2) <= MAX_VEL ** 2)
+       opti.subject_to(theta_dot_k >= -MAX_ANG_VEL)
+       opti.subject_to(theta_dot_k <= MAX_ANG_VEL)
 
-    # ---- boundary conditions --------
-    # state conditions
+    pos = rocket.pos
+    vel = rocket.vel
     opti.subject_to(posx[0] == pos.x) # start at current position.
     opti.subject_to(posy[0] == pos.y) # start at current position.
     opti.subject_to(velx[0] == vel.x) # with current vel
     opti.subject_to(vely[0] == vel.y) # with current vel
+    opti.subject_to(theta[0] == rocket.angle)
+    opti.subject_to(theta_dot[0] == rocket.angular_vel)
+
     opti.subject_to(posx[-1] == waypnts[0].x)  # finish at target
     opti.subject_to(posy[-1] == waypnts[0].y)  # finish at target
+    opti.subject_to(velx[-1] == 0) # with 0 vel
+    opti.subject_to(vely[-1] == 0) # with 0 vel
+    opti.subject_to(Uboost >= 0)
+    opti.subject_to(Uboost <= 1)
+    opti.subject_to(Uyaw >= -1)
+    opti.subject_to(Uyaw <= 1)
 
-    # ---- initial values for solver ---
-    opti.set_initial(velx, -1)
-
-    # ---- solve NLP              ------
     p_opts = dict(print_time=False, verbose=False)
     s_opts = dict(print_level=0, tol=1e-4)
-    opti.solver("ipopt", p_opts, s_opts) # set numerical backend
-    # budget of ~18ms.
-    sol = opti.solve()   # actual solve
-    global U_sol, u_idx
-    U_sol = sol.value(U)
-    u_idx = 0
-
-def progress_input(waypnts):
-    global pos, vel, accel
-    global vel_limit, accel_thrust, vel_dampening
-    # Number of frames to compute ahead of time, the horizon width.
-    N = 250 
-    NW = len(waypnts)
-    opti = Opti()
-
-    # ---- decision variables ---------
-    # state trajectory
-    X       = opti.variable(4,N+1)
-    posx    = X[0,:]
-    posy    = X[1,:]
-    velx    = X[2,:]
-    vely    = X[3,:]
-    # ---- progress variables ---------
-    # each waypoint has a 'progress' state. This state begins at 1, and will be
-    # decremented to 0 when the waypoint is marked as completed.
-    progress = opti.variable(NW,N+1)
-    progress_change = opti.variable(NW,N+1)
-    slack_dist = opti.variable(NW,N+1)
-    allowed_slack = opti.parameter()
-    opti.set_value(allowed_slack, wp_thresh)
-    # ---- control variables ---------
-    # acceleration x,y
-    U = opti.variable(2,N)
-    Ux = U[0,:]
-    Uy = U[1,:]
-
-    # ---- objective          ---------
-    opti.minimize((posx[-1] - waypnts[-1].x) ** 2 + (posy[-1] - waypnts[-1].y) ** 2)
-
-    # f = lambda x,u: vertcat(x[1],u-x[1]) # dx/dt = f(x,u)
-    xdot = lambda x, u: vertcat(x[1], x[1] + u[0])
-
-    for k in range(N):
-       # ---- dynamic constraints --------
-       x_k = X[:,k]
-       posx_k = x_k[0]
-       posy_k = x_k[1]
-       velx_k = x_k[2]
-       vely_k = x_k[3]
-       U_k = U[:,k]
-       Ux_k = U_k[0]
-       Uy_k = U_k[1]
-       x_next = vertcat(
-               posx_k + velx_k,
-               posy_k + vely_k,
-               (velx_k + Ux_k) * vel_dampening,
-               (vely_k + Uy_k) * vel_dampening)
-       # Apply dynamics as a constraint
-       opti.subject_to(X[:,k+1] == x_next)
-       # Enforce physics limits
-       opti.subject_to((velx_k ** 2 + vely_k ** 2) <= vel_limit ** 2)
-       # Enforce actuator limits
-       opti.subject_to((Ux_k ** 2 + Uy_k ** 2) <= accel_thrust ** 2)
-
-       # ---- progress constraints --------
-       prog_k = progress[:, k]
-       prog_change_k = progress_change[:, k]
-       prog_next = prog_k - prog_change_k
-       opti.subject_to(progress[:, k+1] == prog_next)
-       for j in range(NW):
-           # CPC
-           waypnt_pos = waypnts[j]
-           dist_to_waypnt = (posx_k - waypnt_pos.x) ** 2 + (posy_k - waypnt_pos.y) ** 2
-           # allowed_slack = slack_dist[j, k]
-           opti.subject_to(prog_change_k[j] * (dist_to_waypnt * allowed_slack) == 0)
-           opti.subject_to(opti.bounded(0, allowed_slack, wp_thresh))
-           # enforce waypoints are visited in order.
-           if j < NW - 1:
-               opti.subject_to(prog_k[j] <= prog_k[j+1])
-       opti.subject_to(opti.bounded(0, prog_k, 1))
-       opti.subject_to(opti.bounded(0, prog_change_k, 1))
-
-    # ---- boundary conditions --------
-    # state conditions
-    opti.subject_to(posx[0] == pos.x) # start at current position.
-    opti.subject_to(posy[0] == pos.y) # start at current position.
-    opti.subject_to(velx[0] == vel.x) # with current vel
-    opti.subject_to(vely[0] == vel.y) # with current vel
-    # opti.subject_to(posx[-1] == waypnts[-1].x)  # finish at target
-    # opti.subject_to(posy[-1] == waypnts[-1].y)  # finish at target
-    # opti.subject_to(velx[-1] == 0) # at a stop
-    # opti.subject_to(vely[-1] == 0) # at a stop
-    # opti.subject_to(velx[-2] == 0) # at a stop
-    # opti.subject_to(vely[-2] == 0) # at a stop
-
-    # ---- progress conditions --------
-    # progress variables all begin at 1
-    opti.subject_to(progress[:, 0] == 1)
-    # and all end at 0, meaning all waypoints have been visited.
-    opti.subject_to(progress[:, -1] == 0)
-
-    # ---- initial values for solver ---
-    opti.set_initial(velx, -1)
-
-    # ---- solve NLP              ------
-    p_opts = dict(print_time=False, verbose=False)
-    s_opts = dict(print_level=0)
-    opti.solver("ipopt", p_opts, s_opts) # set numerical backend
-    # budget of ~18ms.
-    sol = opti.solve()   # actual solve
-    print("progress sol", sol.value(progress))
+    opti.solver("ipopt", p_opts, s_opts)
+    sol = opti.solve()
     global U_sol, u_idx
     U_sol = sol.value(U)
     u_idx = 0
 
 def rocket_ideal_input(ctrl):
-    pass
+    global u_idx
+    if u_idx is None:
+        return
+    background(100)
+    U_k = U_sol[:,u_idx]
+    Uyaw_k = U_k[0]
+    Uboost_k = U_k[1]
+    ctrl.yaw = Uyaw_k
+    ctrl.hold_boost = Uboost_k > 0.01
+    u_idx += 1
+    if u_idx >= U_sol.shape[1]-1:
+        u_idx = None
 
 def draw():
     global path, rocket, controller
     background(50)
     controller.set_from_user();
+    rocket_ideal_input(controller)
     rocket.updateWithInput(controller);
     path.update(rocket.pos)
     path.draw()
@@ -265,8 +183,6 @@ class Rocket:
         # Clamp angular vel.
         if abs(self.angular_vel) > MAX_ANG_VEL:
             self.angular_vel = MAX_ANG_VEL * (1 if self.angular_vel > 0 else -1)
-        elif abs(self.angular_vel) < 0.00001:
-            self.angular_vel = 0
         self.angle += self.angular_vel
         if self.angle > PI:
             self.angle = -PI
@@ -374,3 +290,4 @@ def diamondPath():
 
 if __name__ == '__main__':
     run()
+
